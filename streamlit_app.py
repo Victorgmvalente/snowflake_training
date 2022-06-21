86b2a62cb9fbacf9ebf916b09a889e674db89990
Victorgmvalente <victorgabriel_valente@hotmail.com>
2022-06-21 15:24:44 -0300

    refactor code addind try to fruityvice api

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,15 +27,17 @@ streamlit.dataframe(fruits_to_show)
 
 #New Section to display fruitvice api response
 streamlit.header('Fruitvice Fruit Advice!')
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+try:
+    fruit_choice = streamlit.text_input('What fruit would you like information about?')
+    if not fruit_choice:
+        streamlit.error("Please select a fruit to get information.")
+    else:
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+        fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+        streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+    streamlit.error()
 
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-# normalize json using pandas
-fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
-# display the normalized data on the page
-streamlit.dataframe(fruityvice_normalized)
 
 # don't run anything past here while we troubleshoot
 streamlit.stop()
