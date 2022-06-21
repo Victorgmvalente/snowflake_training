1d5efba1e9d7a4ebc2ae1d8cf227afc5f95e8161
Victorgmvalente <victorgabriel_valente@hotmail.com>
2022-06-21 11:34:48 -0300

    normilized json format to df

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,4 +26,7 @@ streamlit.header('Fruitvice Fruit Advice!')
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+# normalize json using pandas
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# display the normalized data on the page
+streamlit.dataframe(fruityvice_normalized)
